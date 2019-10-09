1363d17b671c19402ac27cbca91cca27d5100f69
Hannah Malm <hm433314@ohio.edu>
2019-10-09 12:05:53 -0400

    making employee page

---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group5Project.Models
+{
+    public class Employee
+    {
+        public int employeeID { get; set; }
+        public  string  employeeFirstName { get; set; }
+        public string employeeLastName { get; set; }
+        public string employeeBusinessUnit { get; set; }
+        public DateTime employeeHireDate { get; set; }
+        public string employeeTitle{ get; set; }
+
+
+    }
+}
