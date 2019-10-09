54ed0e0db6fb2a210d4269963241dc9dc4f131e2
Diso <nd141014@ohio.edu>
2019-10-09 09:57:32 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Group5Project.Startup))]
+namespace Group5Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
